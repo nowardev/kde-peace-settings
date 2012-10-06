958b253b3fb7b063288b5acf4073340f31326a79
sem <sem@kubuntu.(none)>
2012-10-06 09:09:58 +0200

    unity task manager with dock helpers enabled

--- Plama-Panels-Collection/usr/share/kde4/apps/plasma/layout-templates/org.kde.plasma-desktop.unityPanel/contents/layout.js
+++ Plama-Panels-Collection/usr/share/kde4/apps/plasma/layout-templates/org.kde.plasma-desktop.unityPanel/contents/layout.js
@@ -18,12 +18,12 @@
 ///UNITY LIKE PANELS
 function remove()
 {
-  for (i in panelIds) {
-        
-
-	panelById(panelIds[i]).remove()
-    }
-  
+	for (i in panelIds) {
+		
+		
+		panelById(panelIds[i]).remove()
+	}
+	
 }
 //////////////////////////////////////////////////////////
 
@@ -31,22 +31,22 @@ function remove()
 /////function remove standard///////////////////////////////
 function RemoveOldPanels()
 {
-    for (i in panelIds) {
-       // panelById(panelIds[i]).remove()
-       p = panelById(panelIds[i]);
-       if (typeof p === "undefined") {
-       print("E: Couldn't find first panel");
-       remove() //load the remove function in case of problems
-       }
-        else 	{
-	  panelById(panelIds[i]).remove()
-	  	} 
-                                    }
+	for (i in panelIds) {
+		// panelById(panelIds[i]).remove()
+		p = panelById(panelIds[i]);
+		if (typeof p === "undefined") {
+			print("E: Couldn't find first panel");
+			remove() //load the remove function in case of problems
+	}
+	else 	{
+		panelById(panelIds[i]).remove()
+	} 
+}
 }
 
 // RemoveOldPanels() //load the function 
 // RemoveOldPanels() // reload again to prevent plasma crash load the function 
- 
+
 
 
 var screenrect = screenGeometry(0) // get information about your screen h w 
@@ -67,58 +67,97 @@ function unity(){
 		panel.height = screenrect.height/30;
 	}
 	
-			var kwinbutton = panel.addWidget ("kwinbutton")
-			kwinbutton.currentConfigGroup = new Array('Configuration')
-			kwinbutton.writeConfig("ActionType", "1")
-			kwinbutton.writeConfig("Maximized", "0")
-			
-			var menubar = panel.addWidget("menubar")
-			
-			var kwinbutton = panel.addWidget ("kwinbutton")
-			// kwinbutton.currentConfigGroup = new Array('Configuration')
-			kwinbutton.writeConfig("ActionType", "2")
-			kwinbutton.writeConfig("Maximized", "1")
-			
-			var kwinbutton = panel.addWidget ("kwinbutton")
-			// kwinbutton.currentConfigGroup = new Array('Configuration')
-			kwinbutton.writeConfig("ActionType", "3")
-			kwinbutton.writeConfig("Maximized", "1")
-			
-			// var panelspacer = panel.addWidget ("panelspacer_internal")
-			
-			var kwinbutton = panel.addWidget ("kwinbutton")
-			// kwinbutton.currentConfigGroup = new Array('Configuration')
-			kwinbutton.writeConfig("ActionType", "4")
-			kwinbutton.writeConfig("Maximized", "1")
-			
-			var systemtray = panel.addWidget("systemtray")
-			
-			var clock = panel.addWidget("digital-clock")
-			clock.writeConfig("showDate", "false")
-			clock.writeConfig("showDay", "true")
-			clock.writeConfig("showSeconds", "false")
-			clock.writeConfig("showYear", "true")
-			clock.writeConfig("showShadow", "false")
-			clock.writeConfig("showYear", "true")
-			//clock.writeConfig("showTimezone", "true")
-			clock.writeConfig("plainClockFont", "Serif,12,-1,5,75,0,0,0,0,0")
-			clock.writeConfig("useCustomColor", "true")
-			clock.writeConfig("plainClockColor", "255,255,255")
-			clock.writeConfig("plainClockDrawShadow", "false")
-			
-			
-			
-// 			var telepat = panel.addWidget("org.kde.telepathy-presence")
-			
-			var launcher = panel.addWidget("simplelauncher")
-			launcher.writeConfig("format", "Description")
-			launcher.writeConfig("showMenuTitles", "true")
-			launcher.writeConfig("views", "Settings,SwitchUser,SaveSession,LockScreen,Logout,Standby,SuspendDisk,SuspendRAM,Restart,Shutdown")
-			launcher.writeConfig("icon", "system-shutdown")
-			
-			// panel.reloadConfig()
-			
-			
+	var widget = "kwinbutton"
+	/*check if the widget is installed and add if it is to your panel*/
+	if (knownWidgetTypes.indexOf(widget) > -1) {
+		var widget = panel.addWidget(widget)
+		widget.writeConfig("ActionType", "1")
+		widget.writeConfig("Maximized", "1")
+		
+	} 
+	else {
+		print("your system doesn't provide "+widget )
+		
+	}
+	
+	
+	
+	var menubar = panel.addWidget("menubar")
+	
+	
+	
+	var widget = "kwinbutton"
+	/*check if the widget is installed and add if it is to your panel*/
+	if (knownWidgetTypes.indexOf(widget) > -1) {
+		var widget = panel.addWidget(widget)
+		widget.writeConfig("ActionType", "2")
+		widget.writeConfig("Maximized", "1")
+		
+	} 
+	else {
+		print("your system doesn't provide "+widget )
+		
+	}
+	
+	
+	var widget = "kwinbutton"
+	/*check if the widget is installed and add if it is to your panel*/
+	if (knownWidgetTypes.indexOf(widget) > -1) {
+		var widget = panel.addWidget(widget)
+		widget.writeConfig("ActionType", "3")
+		widget.writeConfig("Maximized", "1")
+		
+	} 
+	else {
+		print("your system doesn't provide "+widget )
+		
+	}
+	
+	// var panelspacer = panel.addWidget ("panelspacer_internal")
+	
+	var widget = "kwinbutton"
+	/*check if the widget is installed and add if it is to your panel*/
+	if (knownWidgetTypes.indexOf(widget) > -1) {
+		var widget = panel.addWidget(widget)
+		widget.writeConfig("ActionType", "4")
+		widget.writeConfig("Maximized", "1")
+		
+	} 
+	else {
+		print("your system doesn't provide "+widget )
+		
+	}
+	
+	
+	
+	var systemtray = panel.addWidget("systemtray")
+	
+	var clock = panel.addWidget("digital-clock")
+	clock.writeConfig("showDate", "false")
+	clock.writeConfig("showDay", "true")
+	clock.writeConfig("showSeconds", "false")
+	clock.writeConfig("showYear", "true")
+	clock.writeConfig("showShadow", "false")
+	clock.writeConfig("showYear", "true")
+	//clock.writeConfig("showTimezone", "true")
+	clock.writeConfig("plainClockFont", "Serif,12,-1,5,75,0,0,0,0,0")
+	clock.writeConfig("useCustomColor", "true")
+	clock.writeConfig("plainClockColor", "255,255,255")
+	clock.writeConfig("plainClockDrawShadow", "false")
+	
+	
+	
+	// 			var telepat = panel.addWidget("org.kde.telepathy-presence")
+	
+	var launcher = panel.addWidget("simplelauncher")
+	launcher.writeConfig("format", "Description")
+	launcher.writeConfig("showMenuTitles", "true")
+	launcher.writeConfig("views", "Settings,SwitchUser,SaveSession,LockScreen,Logout,Standby,SuspendDisk,SuspendRAM,Restart,Shutdown")
+	launcher.writeConfig("icon", "system-shutdown")
+	
+	// panel.reloadConfig()
+	
+	
 	var panel = new Panel
 	if (panelIds.length == 2) {
 		// we are the only panel, so set the location for the user panel.location = 'bottom'
@@ -127,108 +166,111 @@ function unity(){
 		//panel.hiding= 'windowscover' //"windowscover" or "windowsbelow" autohide
 		
 	}
-			
-			
-			
-			
-			panel.height = screenrect.height/18
-			
-			var launcher = panel.addWidget("homerunlauncher")
-			
-			// var launcher = panel.addWidget("lancelot_part")
-			// launcher.writeConfig("iconLocation", "start-here-kde")
-			// launcher.writeConfig("iconClickActivation", "true")
-			// launcher.writeConfig("contentsExtenderPosition", "1")
-			// launcher.writeConfig("searchHistory", "firefox")
-			// launcher.writeConfig("showSearchBox", "true")
-			// launcher.writeConfig("partData", "model=FavoriteApplications&type=list&version=1.0\nmodel=Folder%20applications%3A%2FOffice%2F&type=list&version=1.0\nmodel=Folder%20applications%3A%2FMultimedia%2F&type=list&version=1.0\nmodel=Folder%20applications%3A%2FInternet%2F&type=list&version=1.0\nmodel=System&type=list&version=1.0\n") 
-			
-			
-			var launcher = panel.addWidget("lancelot_part")
-			launcher.writeConfig("iconLocation", "drive-harddisk")
-			launcher.writeConfig("iconClickActivation", "true")
-			launcher.writeConfig("contentsExtenderPosition", "1")
-			launcher.writeConfig("searchHistory", "firefox")
-			launcher.writeConfig("showSearchBox", "true")
-			launcher.writeConfig("partData","model=Devices%2FRemovable&type=list&version=1.0\nmodel=Places&type=list&version=1.0\nmodel=Devices%2FFixed&type=list&version=1.0\n") 
-			
-			var quick = panel.addWidget("quickaccess") 
-			
-			
-			var icontasks = panel.addWidget("icontasks")
-			icontasks.writeConfig("groupClick","1")
-			icontasks.writeConfig("iconScale","49")
-			icontasks.writeConfig("launcherIcons","false")
-			icontasks.writeConfig("maxRows","1")
-			icontasks.writeConfig("mediaButtons","true")
-			icontasks.writeConfig("middleClick","0")
-			icontasks.writeConfig("previewSize","200")
-			icontasks.writeConfig("rotate","false")
-			icontasks.writeConfig("showOnlyCurrentActivity","false")
-			icontasks.writeConfig("showOnlyCurrentDesktop","false")
-			icontasks.writeConfig("showOnlyCurrentScreen","false")
-			icontasks.writeConfig("showProgress","true")
-			icontasks.writeConfig("showSeparator","0")
-			icontasks.writeConfig("sortingStrategy","2")
-			icontasks.writeConfig("spacing","0")
-			icontasks.writeConfig("style","2")
-			icontasks.writeConfig("toolTips","1")
-			icontasks.writeConfig("unity","true")
-			//icontasks.writeConfig("Enabled","true")
-			icontasks.currentConfigGroup = new Array('Launchers') //CONFIGURE THE LAUNCHERS 
-			icontasks.writeConfig("Items","file:///usr/share/applications/firefox.desktop?wmClass=Firefox,file:///usr/share/applications/kde4/kate.desktop?wmClass=Kate,file:///usr/share/applications/kde4/konversation.desktop?wmClass=Konversation,file:////usr/share/applications/kde4/dolphin.desktop?wmClass=Dolphin,file:///usr/share/applications/chromium-browser.desktop?wmClass=Chromium-browser,file:///usr/share/applications/kde4/konsole.desktop?wmClass=Konsole,file:///usr/share/applications/kde4/ksnapshot.desktop?wmClass=Ksnapshot,file:///usr/share/applications/kde4/systemsettings.desktop?wmClass=Systemsettings,file:///usr/share/applications/kde4/muon-installer.desktop")
-			
-			
-			
-			// var opendocuments = panel.addWidget("lancelot_part")
-			
-			// opendocuments.writeConfig("iconClickActivation", "true")
-			// opendocuments.writeConfig("contentsExtenderPosition", "1")
-			// opendocuments.writeConfig("iconLocation", "document-edit") 
-			// opendocuments.writeConfig("showSearchBox", "true")
-			// opendocuments.writeConfig("partData", "model=OpenDocuments&type=list&version=1.0\n") 
-			
-			
-			
-			
-			
-			
-			// var newdocuments = panel.addWidget("lancelot_part")
-			// newdocuments.writeConfig("iconLocation", "document-new")
-			// newdocuments.writeConfig("iconClickActivation", "true")
-			// newdocuments.writeConfig("contentsExtenderPosition", "1")
-			
-			// newdocuments.writeConfig("showSearchBox", "true")
-			// newdocuments.writeConfig("partData", "model=NewDocuments&type=list&version=1.0\n") 
-			
-			// var messagescontact = panel.addWidget("lancelot_part")
-			// messagescontact.writeConfig("iconLocation", "im-message-new")
-			// messagescontact.writeConfig("iconClickActivation", "true")
-			// messagescontact.writeConfig("contentsExtenderPosition", "1")
-			// messagescontact.writeConfig("showSearchBox", "true")
-			// messagescontact.writeConfig("partData", "model=Messages&type=list&version=1.0\nmodel=Contacts&type=list&version=1.0") 
-			
-			panel.addWidget("pastebin")
-			panel.addWidget("showdesktop")
-			
-			var pager = panel.addWidget("pager")
-			pager.writeGlobalConfig("rows", "2")
-			
-			panel.addWidget("trash")
-			
-			sleep(4000)
-			panel.hiding= 'autohide' //"windowscover" or "windowsbelow" 
-			
-			//var lockout = panel.addWidget("lockout")
-			//lockout.writeConfig("showHibernateButton","true")
-			//lockout.writeConfig("showLogoutButton","true")
-			//lockout.writeConfig("showLockButton","false")
-			//lockout.writeConfig("showSleepButton","false")
-			//lockout.writeConfig("showSwitchUserButton","false")
-			
-			
-			
-			
+	
+	
+	
+	
+	panel.height = screenrect.height/18
+	
+	var launcher = panel.addWidget("homerunlauncher")
+	
+	// var launcher = panel.addWidget("lancelot_part")
+	// launcher.writeConfig("iconLocation", "start-here-kde")
+	// launcher.writeConfig("iconClickActivation", "true")
+	// launcher.writeConfig("contentsExtenderPosition", "1")
+	// launcher.writeConfig("searchHistory", "firefox")
+	// launcher.writeConfig("showSearchBox", "true")
+	// launcher.writeConfig("partData", "model=FavoriteApplications&type=list&version=1.0\nmodel=Folder%20applications%3A%2FOffice%2F&type=list&version=1.0\nmodel=Folder%20applications%3A%2FMultimedia%2F&type=list&version=1.0\nmodel=Folder%20applications%3A%2FInternet%2F&type=list&version=1.0\nmodel=System&type=list&version=1.0\n") 
+	
+	
+	var launcher = panel.addWidget("lancelot_part")
+	launcher.writeConfig("iconLocation", "drive-harddisk")
+	launcher.writeConfig("iconClickActivation", "true")
+	launcher.writeConfig("contentsExtenderPosition", "1")
+	launcher.writeConfig("searchHistory", "firefox")
+	launcher.writeConfig("showSearchBox", "true")
+	launcher.writeConfig("partData","model=Devices%2FRemovable&type=list&version=1.0\nmodel=Places&type=list&version=1.0\nmodel=Devices%2FFixed&type=list&version=1.0\n") 
+	
+	var quick = panel.addWidget("quickaccess") 
+	
+	
+	var icontasks = panel.addWidget("icontasks")
+	icontasks.writeConfig("groupClick","1")
+	icontasks.writeConfig("iconScale","49")
+	icontasks.writeConfig("launcherIcons","false")
+	icontasks.writeConfig("maxRows","1")
+	icontasks.writeConfig("mediaButtons","true")
+	icontasks.writeConfig("middleClick","0")
+	icontasks.writeConfig("previewSize","200")
+	icontasks.writeConfig("rotate","false")
+	icontasks.writeConfig("showOnlyCurrentActivity","false")
+	icontasks.writeConfig("showOnlyCurrentDesktop","false")
+	icontasks.writeConfig("showOnlyCurrentScreen","false")
+	icontasks.writeConfig("showProgress","true")
+	icontasks.writeConfig("showSeparator","0")
+	icontasks.writeConfig("sortingStrategy","2")
+	icontasks.writeConfig("spacing","0")
+	icontasks.writeConfig("style","2")
+	icontasks.writeConfig("toolTips","1")
+	icontasks.writeConfig("unity","true")
+	//icontasks.writeConfig("Enabled","true")
+	icontasks.currentConfigGroup = new Array('DockManager') // CONFIGURE DOCKERS HELPERS
+	icontasks.writeConfig("Enabled","true")
+	icontasks.writeConfig("EnabledHelpers",userhome+"/.local/share/dockmanager/scripts/kate.py,"+userhome+"/.local/share/dockmanager/scripts/konqueror.py,"+userhome+"/.local/share/dockmanager/scripts/krdc_menus.py,"+userhome+"/.local/share/dockmanager/scripts/rekonq.py,"+userhome+"/.local/share/dockmanager/scripts/akonadi.py,"+userhome+"/.local/share/dockmanager/scripts/firefox.py,"+userhome+"/.local/share/dockmanager/scripts/konsole.py,"+userhome+"/.local/share/dockmanager/scripts/dolphin.py,"+userhome+"/.local/share/dockmanager/scripts/kopete.py,"+userhome+"/.local/share/dockmanager/scripts/amarok.py")
+	icontasks.currentConfigGroup = new Array('Launchers') //CONFIGURE THE LAUNCHERS 
+	icontasks.writeConfig("Items","file:///usr/share/applications/firefox.desktop?wmClass=Firefox,file:///usr/share/applications/kde4/kate.desktop?wmClass=Kate,file:///usr/share/applications/kde4/konversation.desktop?wmClass=Konversation,file:////usr/share/applications/kde4/dolphin.desktop?wmClass=Dolphin,file:///usr/share/applications/chromium-browser.desktop?wmClass=Chromium-browser,file:///usr/share/applications/kde4/konsole.desktop?wmClass=Konsole,file:///usr/share/applications/kde4/ksnapshot.desktop?wmClass=Ksnapshot,file:///usr/share/applications/kde4/systemsettings.desktop?wmClass=Systemsettings,file:///usr/share/applications/kde4/muon-installer.desktop")
+	
+	
+	
+	// var opendocuments = panel.addWidget("lancelot_part")
+	
+	// opendocuments.writeConfig("iconClickActivation", "true")
+	// opendocuments.writeConfig("contentsExtenderPosition", "1")
+	// opendocuments.writeConfig("iconLocation", "document-edit") 
+	// opendocuments.writeConfig("showSearchBox", "true")
+	// opendocuments.writeConfig("partData", "model=OpenDocuments&type=list&version=1.0\n") 
+	
+	
+	
+	
+	
+	
+	// var newdocuments = panel.addWidget("lancelot_part")
+	// newdocuments.writeConfig("iconLocation", "document-new")
+	// newdocuments.writeConfig("iconClickActivation", "true")
+	// newdocuments.writeConfig("contentsExtenderPosition", "1")
+	
+	// newdocuments.writeConfig("showSearchBox", "true")
+	// newdocuments.writeConfig("partData", "model=NewDocuments&type=list&version=1.0\n") 
+	
+	// var messagescontact = panel.addWidget("lancelot_part")
+	// messagescontact.writeConfig("iconLocation", "im-message-new")
+	// messagescontact.writeConfig("iconClickActivation", "true")
+	// messagescontact.writeConfig("contentsExtenderPosition", "1")
+	// messagescontact.writeConfig("showSearchBox", "true")
+	// messagescontact.writeConfig("partData", "model=Messages&type=list&version=1.0\nmodel=Contacts&type=list&version=1.0") 
+	
+	panel.addWidget("pastebin")
+	panel.addWidget("showdesktop")
+	
+	var pager = panel.addWidget("pager")
+	pager.writeGlobalConfig("rows", "2")
+	
+	panel.addWidget("trash")
+	
+	sleep(4000)
+	panel.hiding= 'autohide' //"windowscover" or "windowsbelow" 
+	
+	//var lockout = panel.addWidget("lockout")
+	//lockout.writeConfig("showHibernateButton","true")
+	//lockout.writeConfig("showLogoutButton","true")
+	//lockout.writeConfig("showLockButton","false")
+	//lockout.writeConfig("showSleepButton","false")
+	//lockout.writeConfig("showSwitchUserButton","false")
+	
+	
+	
+	
 }
 
 
@@ -238,4 +280,4 @@ unity()
 
 
 
- 
+
